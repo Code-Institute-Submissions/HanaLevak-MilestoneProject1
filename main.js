466106c56052a145696d30a32d67f93a791ddb3e
HanaLevak <hanna.levak@gmail.com>
2020-08-06 11:22:18 +0000

    jumbotrons ready
    Signed-off-by: HanaLevak <hanna.levak@gmail.com>

--- main.js
+++ main.js
@@ -1,12 +1,15 @@
-$('.carousel')(autoplay) (repeat)
+$('.carousel')(autoplay devicePixelRatio event) (repeat)
 const newLocal = <button type="submit" class="btn btn-light">Sign in</button>;
 $(document).ready(function(_b)
   $('.header').height($(window).height(main.js,{{"."ts}}
 
-  
+computed: {
+  jumbotron images()
+    return {
+      backgroundImage: `url${require('../assets/images')
+    
 
- webkitURL ("assets/images/Everybody's welcome.jpg")..type Image)
-   font-family-sans-serif:{
+   font-family-sans-serif(
  //Safari for OS X and iOS (San Francisco)
   -apple-system,
   // Chrome < 56 for OS X (San Francisco)
